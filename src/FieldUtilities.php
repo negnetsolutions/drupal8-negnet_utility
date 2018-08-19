8a2e45124ccba898f9907a8e737425ca0e4f706c
Andrew Johnson <andy@negnetsolutions.com>
2018-08-19 17:28:31 -0500

    add fieldHasChildren

--- src/FieldUtilities.php
+++ src/FieldUtilities.php
@@ -4,48 +4,72 @@ namespace Drupal\negnet_utility;
 
 use Drupal\Core\Render\Element;
 
-class FieldUtilities {
+class FieldUtilities
+{
 
-  private static $numberMappings = array('one','two','three','four','five','six');
+    private static $numberMappings = array('one','two','three','four','five','six');
 
-  /**
+    /**
+   * Returns true or false if element has children
+   */
+    public static function fieldHasChildren($element, $field) 
+    {
+        if (is_array($element)) {
+            if (isset($element[$field]) && isset($element[$field][0])) {
+                return true;
+            }
+        }
+        else if ($element->hasField($field)) {
+            if ($element->get($field)->isEmpty()) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
    * Returns all of a render array's children.
    */
-  public static function elementChildren(array &$elements){
-    $children = Element::children($elements);
-    return self::sub_array($elements,$children);
-  }
+    public static function elementChildren(array &$elements)
+    {
+        $children = Element::children($elements);
+        return self::sub_array($elements, $children);
+    }
 
-  /**
+    /**
    * Returns all of the field's values
    */
-  public static function fieldChildren(&$elements) {
-    $children = [];
-    foreach($elements as $e) {
-      $children[] = $e->value;
+    public static function fieldChildren(&$elements) 
+    {
+        $children = [];
+        foreach($elements as $e) {
+            $children[] = $e->value;
+        }
+
+        return $children;
     }
 
-    return $children;
-  }
-
-  /*
-   * Returns a portion of an array based on array keys
-   */
-  public static function sub_array(array $haystack, array $needle){
-    return array_intersect_key($haystack, array_flip($needle));
-  }
-
-
-  /*
-   * Converts an integer into a named number
-   * or returns false if can't find a mapping
-   */
-  public static function numberToName(int $number){
-    $number -= 1;
-
-    if(isset(self::$numberMappings[$number])){
-      return self::$numberMappings[$number];
+    /*
+    * Returns a portion of an array based on array keys
+    */
+    public static function sub_array(array $haystack, array $needle)
+    {
+        return array_intersect_key($haystack, array_flip($needle));
     }
-    return false;
-  }
+
+
+    /*
+    * Converts an integer into a named number
+    * or returns false if can't find a mapping
+    */
+    public static function numberToName(int $number)
+    {
+        $number -= 1;
+
+        if(isset(self::$numberMappings[$number])) {
+            return self::$numberMappings[$number];
+        }
+        return false;
+    }
 }
