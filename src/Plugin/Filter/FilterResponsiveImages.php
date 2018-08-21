bc09c7d6a4b323ba713d0d83c652bb67f315c508
Andrew Johnson <andy@negnetsolutions.com>
2018-08-20 21:48:31 -0500

    more robust image filter

--- src/Plugin/Filter/FilterResponsiveImages.php
+++ src/Plugin/Filter/FilterResponsiveImages.php
@@ -14,63 +14,83 @@ use Drupal\Core\Form\FormStateInterface;
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
  * )
  */
-class FilterResponsiveImages extends FilterBase {
-  public function process($text, $langcode) {
+class FilterResponsiveImages extends FilterBase
+{
+    public function process($text, $langcode) 
+    {
 
-    if( preg_match_all('/<img[\\w\\W]+?class="([a-zA-Z-_0-9]+)"[\\w\\W]+?src="\/sites\/default\/files\/([\\w\\s-?=\\.\/]+)"[\\w\\W]+?\/>/u', $text, $matches_code) ){
-      foreach($matches_code[0] as $ci => $code){
-        //get image filename
-        $img = $matches_code[2][$ci];
-        $class = $matches_code[1][$ci];
-        $classes = explode(' ',$class);
+        $dom = new \DOMDocument;
+        $dom->loadHTML($text);
+        $images = $dom->getElementsByTagName('img');
+        foreach ($images as $image) {
+            $classes = explode(' ', $image->getAttribute('class'));
+            if (!strstr($classes, 'responsive-image')) {
+                //need to process
+                $src = $image->getAttribute('src');
+                $imgf = strstr($src, '?', true);
+                if($imgf !== false) {
+                    $src = $imgf;
+                }
 
-        //remove any parameters
-        $imgf = strstr($img, '?', true);
-        if($imgf !== false){
-          $img = $imgf;
+                $src = str_replace('/sites/default/files', '', $src);
+
+                $uri = "public:/".$src;
+
+                $new_image = array(
+                  '#responsive_image_style_id' => isset($this->settings['responsive_image_style']) ? $this->settings['responsive_image_style'] : null,
+                  '#uri' => $uri,
+                  '#theme' => 'responsive_image',
+                  '#width' => null,
+                  '#height' => null,
+                  '#attributes' => [
+                    'class' => $classes,
+                  ],
+                );
+
+                $i = \Drupal::service('image.factory')->get($uri);
+                if ($i->isValid()) {
+                      $new_image['#height'] = $i->getHeight();
+                      $new_image['#width'] = $i->getWidth();
+                }
+
+                $replacement = (string) \Drupal::service('renderer')->render($new_image);
+                $this->setInnerHTML($dom, $image, $replacement);
+            }
         }
 
-        $uri = "public://".$img;
+        $text = $dom->saveHTML();
 
-        $image = array(
-          '#responsive_image_style_id' => isset($this->settings['responsive_image_style']) ? $this->settings['responsive_image_style'] : null,
-          '#uri' => $uri,
-          '#theme' => 'responsive_image',
-          '#width' => null,
-          '#height' => null,
-          '#attributes' => [
-            'class' => $classes,
-          ],
+        return new FilterProcessResult($text);
+    }
+
+    protected function setInnerHTML(&$dom, $el, $newInnerHTML)
+    {
+        $tmpDoc = new \DOMDocument();
+        $tmpDoc->loadHTML($newInnerHTML);
+        foreach ($tmpDoc->getElementsByTagName('img') as $node) {
+            $newElement = $dom->importNode($node, true);
+            $el->parentNode->insertBefore($newElement, $el);
+        }
+        $el->parentNode->removeChild($el);
+
+        return $newElement;
+    }
+
+    public function settingsForm(array $form, FormStateInterface $form_state) 
+    {
+        $image_styles = \Drupal::entityTypeManager()->getStorage('responsive_image_style')->loadMultiple();
+        $responsive_image_styles = array();
+        foreach($image_styles as $id => $style){
+            $responsive_image_styles[$id] = $style->label();
+        }
+
+        $form['responsive_image_style'] = array(
+        '#type' => 'select',
+        '#title' => $this->t('Responsive Image Style to Use'),
+        '#options' => $responsive_image_styles,
+        '#required' => true,
+        '#default_value' => isset($this->settings['responsive_image_style']) ? $this->settings['responsive_image_style'] : '',
         );
-
-        $i = \Drupal::service('image.factory')->get($uri);
-        if($i->isValid()){
-          $image['#height'] = $i->getHeight();
-          $image['#width'] = $i->getWidth();
-        }
-
-        $replacement = \Drupal::service('renderer')->render($image);
-        $text = str_replace($code, $replacement, $text);
-
-      }
+        return $form;
     }
-
-    return new FilterProcessResult($text);
-  }
-  public function settingsForm(array $form, FormStateInterface $form_state) {
-    $image_styles = \Drupal::entityTypeManager()->getStorage('responsive_image_style')->loadMultiple();
-    $responsive_image_styles = array();
-    foreach($image_styles as $id => $style){
-      $responsive_image_styles[$id] = $style->label();
-    }
-
-    $form['responsive_image_style'] = array(
-      '#type' => 'select',
-      '#title' => $this->t('Responsive Image Style to Use'),
-      '#options' => $responsive_image_styles,
-      '#required' => TRUE,
-      '#default_value' => isset($this->settings['responsive_image_style']) ? $this->settings['responsive_image_style'] : '',
-    );
-    return $form;
-  }
 }
