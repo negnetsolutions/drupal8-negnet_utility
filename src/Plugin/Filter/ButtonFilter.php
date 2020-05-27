34a7fb39b916ea7f062be8038647453c2e23d1bf
Andrew Johnson <andy@negnet.co>
2020-05-27 07:38:06 -0500

    added ButtonFilter

---
+++ src/Plugin/Filter/ButtonFilter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\negnet_utility\Plugin\Filter;
+
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+
+/**
+ * @file
+ * Text filter for Negnet Solutions.
+ */
+
+/**
+ * Class ButtonFilter.
+ *
+ * @Filter(
+ *   id = "filter_negnet_buttons",
+ *   title = @Translation("Button Container"),
+ *   description = @Translation("Places button links together in a .button_container."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
+ * )
+ */
+class ButtonFilter extends FilterBase {
+
+  /**
+   * Implements process.
+   */
+  public function process($text, $langcode) {
+
+    $dom = new \DOMDocument();
+    libxml_use_internal_errors(TRUE);
+    $dom->loadHTML(mb_convert_encoding($text, 'HTML-ENTITIES', 'UTF-8'));
+    libxml_clear_errors();
+    $links = $dom->getElementsByTagName('a');
+    foreach ($links as $link) {
+
+      // Get classes.
+      $classes = explode(' ', $link->getAttribute('class'));
+      if (in_array('btn', $classes)) {
+        $parentClasses = explode(' ', $link->parentNode->getAttribute('class'));
+        $parentClasses[] = 'button_container';
+        $parentClasses = array_unique($parentClasses);
+        $link->parentNode->setAttribute('class', trim(implode(' ', $parentClasses)));
+      }
+    }
+
+    $text = $dom->saveHTML();
+
+    return new FilterProcessResult($text);
+  }
+
+}
